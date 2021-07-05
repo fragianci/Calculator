e697396a71195176567b38ae8adad45d658d8858
Francesco Giancipoli <francescogiancipoli15@gmail.com>
2021-07-05 21:54:16 +0200

    Aggiunti gli operandi

--- src/app/operations-panel/operations-panel.component.ts
+++ src/app/operations-panel/operations-panel.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperationsPanelComponent implements OnInit {
   numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  operands = ['+', '-', 'x', '/'];
 
   constructor() { }
 
