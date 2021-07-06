9bf5aed1f7d59e8422717d872c6552c71dea723a
Francesco Giancipoli <francescogiancipoli15@gmail.com>
2021-07-06 21:14:54 +0200

    Aggiunta una bozza di logica in typescript

--- src/app/operations-panel/operations-panel.component.ts
+++ src/app/operations-panel/operations-panel.component.ts
@@ -8,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
 export class OperationsPanelComponent implements OnInit {
   numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   operands = ['+', '-', 'x', '/'];
+  opNumbers: number[] = [];
+  result: number = 0;
+  myOperator: string = ''
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  catchMyNum(number: number){
+    this.opNumbers.push(number);
+    console.log(this.opNumbers)
+  }
+
+  catchOperator(op: string){
+    this.myOperator = op;
+  }
+
+  showResult(){
+    let result: number = 0;
+    if(this.opNumbers.length > 0){
+      switch (this.myOperator){
+        case '+':
+          for(let n of this.opNumbers){
+            result += n;
+          }
+          break;
+        case '-':
+          for(let n of this.opNumbers){
+            result += +n;
+          }
+          break;
+        case 'x':
+          for(let n of this.opNumbers){
+            result += +n;
+          }
+          break;
+        case '/':
+          for(let n of this.opNumbers){
+            result += +n;
+          }
+          break;
+      }
+    }
+    this.result = result;
+
+    console.log(this.result);
   }
 
 }
